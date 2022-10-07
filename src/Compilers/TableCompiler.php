caae815d142c09d76401e0366d209305d77f8b31
Pete Cooper <pete@petecoop.co.uk>
2022-10-07 18:10:10 +0100

    allow table to be an array key

--- src/Compilers/TableCompiler.php
+++ src/Compilers/TableCompiler.php
@@ -48,16 +48,19 @@ class TableCompiler implements Compiler
         $this->dom->loadXML($value);
         $this->crawler = new Crawler($this->dom);
 
-        $this->table = $this->crawler->filter('table|table[table|name="'.$this->name.'"]');
-        $this->header = $this->table->filter('table|table-row')->first();
-        $this->column = $this->table->children('table|table-column')->first();
-        $this->row = $this->table->children('table|table-row')->last();
+        $this->table = $this->crawler->filter('table|table[table|name="' . $this->name . '"]');
+        $this->header = $this->table->filter("table|table-row")->first();
+        $this->column = $this->table->children("table|table-column")->first();
+        $this->row = $this->table->children("table|table-row")->last();
 
         $this->columnTemplate = $this->column->outerHtml();
-        $this->columnStyleTemplate = $this->column->attr('table:style-name');
+        $this->columnStyleTemplate = $this->column->attr("table:style-name");
         $this->columnTarget = $this->column->siblings()->first();
-        $this->headerTemplate = $this->header->children('table|table-cell')->first()->outerHtml();
-        $cell = $this->row->children('table|table-cell')->first();
+        $this->headerTemplate = $this->header
+            ->children("table|table-cell")
+            ->first()
+            ->outerHtml();
+        $cell = $this->row->children("table|table-cell")->first();
         $this->cellTemplate = $cell->outerHtml();
         $this->cellContentTemplate = $cell->html();
     }
@@ -75,7 +78,7 @@ class TableCompiler implements Compiler
         }
 
         // if given an arrayable convert to array
-        if (!is_array($item) && is_object($item) && method_exists($item, 'toArray')) {
+        if (!is_array($item) && is_object($item) && method_exists($item, "toArray")) {
             $item = $item->toArray();
         }
 
@@ -87,12 +90,12 @@ class TableCompiler implements Compiler
         foreach ($keys as $key) {
             $options = $this->options[$key] ?? [];
             if (!is_array($options)) {
-                $options = ['title' => $options];
+                $options = ["title" => $options];
             }
 
             $defaultOptions = [
-                'title' => $this->titleCase($key),
-                'relativeWidth' => 1,
+                "title" => $this->titleCase($key),
+                "relativeWidth" => 1,
             ];
 
             $this->addColumn($key, $options + $defaultOptions);
@@ -101,26 +104,23 @@ class TableCompiler implements Compiler
 
     private function addColumn(string $key, $options)
     {
-        $count = $this->row->children('table|table-cell')->count();
+        $count = $this->row->children("table|table-cell")->count();
 
         $headerNode = $this->header->getNode(0);
         $rowNode = $this->row->getNode(0);
 
-        $style = $this->createColumnStyle($options['relativeWidth'] ?? 1);
+        $style = $this->createColumnStyle($options["relativeWidth"] ?? 1);
 
         if ($count == 1) {
             // if it's the first row then replace
             $this->table->getNode(0)->replaceChild($this->createColumn($style), $this->column->getNode(0));
-            $headerCell = $this->header->children('table|table-cell')->first();
-            $headerNode->replaceChild($this->createHeader($options['title']), $headerCell->getNode(0));
-            $rowCell = $this->row->children('table|table-cell')->first();
+            $headerCell = $this->header->children("table|table-cell")->first();
+            $headerNode->replaceChild($this->createHeader($options["title"]), $headerCell->getNode(0));
+            $rowCell = $this->row->children("table|table-cell")->first();
             $rowNode->replaceChild($this->createCell($key, true), $rowCell->getNode(0));
         } else {
-            $this->table->getNode(0)->insertBefore(
-                $this->createColumn($style),
-                $this->columnTarget->getNode(0)
-            );
-            $headerNode->appendChild($this->createHeader($options['title']));
+            $this->table->getNode(0)->insertBefore($this->createColumn($style), $this->columnTarget->getNode(0));
+            $headerNode->appendChild($this->createHeader($options["title"]));
             $rowNode->appendChild($this->createCell($key));
         }
     }
@@ -142,20 +142,28 @@ class TableCompiler implements Compiler
             return $this->columnStyles[$relativeWidth];
         }
 
-        $name = 'TableRelativeWidth' . $relativeWidth;
+        $name = "TableRelativeWidth" . $relativeWidth;
 
-        $styleParent = $this->crawler->filter('office|automatic-styles')->first();
-        $count = $styleParent->filter('style|style[style|name="'.$name.'"]')->count();
+        $styleParent = $this->crawler->filter("office|automatic-styles")->first();
+        $count = $styleParent->filter('style|style[style|name="' . $name . '"]')->count();
         if ($count) {
             $this->columnStyles[$relativeWidth] = $name;
             return $name;
         }
 
-        $styleParent->getNode(0)->appendChild($this->createFragment(
-            '<style:style style:name="'.$name.'" style:family="table-column">' .
-                '<style:table-column-properties style:rel-column-width="'.$relativeWidth.'000*" />' .
-            '</style:style>'
-        ));
+        $styleParent
+            ->getNode(0)
+            ->appendChild(
+                $this->createFragment(
+                    '<style:style style:name="' .
+                        $name .
+                        '" style:family="table-column">' .
+                        '<style:table-column-properties style:rel-column-width="' .
+                        $relativeWidth .
+                        '000*" />' .
+                        "</style:style>"
+                )
+            );
 
         $this->columnStyles[$relativeWidth] = $name;
 
@@ -171,13 +179,18 @@ class TableCompiler implements Compiler
 
     private function createCell(string $key, bool $firstCell = false): DOMDocumentFragment
     {
-        $itemKey = '$' . $this->key . '_item';
+        $itemKey = '$' . preg_replace("/\W/", "", $this->key) . "_item";
         $value = "{{ {$itemKey}['{$key}'] ?? '' }}";
 
         if ($firstCell) {
-            $value = '@beforerow@foreach($'.$this->key.' as '.$itemKey.')@endbeforerow'
-                . $value
-                . '@afterrow@endforeach@endafterrow';
+            $value =
+                '@beforerow@foreach($' .
+                $this->key .
+                " as " .
+                $itemKey .
+                ")@endbeforerow" .
+                $value .
+                "@afterrow@endforeach@endafterrow";
         }
 
         $content = preg_replace('/{{\s*\$row\s*}}/', $value, $this->cellContentTemplate);
@@ -198,7 +211,7 @@ class TableCompiler implements Compiler
 
     private function titleCase(string $value): string
     {
-        $value = str_replace('_', ' ', $value);
-        return mb_convert_case($value, MB_CASE_TITLE, 'UTF-8');
+        $value = str_replace("_", " ", $value);
+        return mb_convert_case($value, MB_CASE_TITLE, "UTF-8");
     }
 }
